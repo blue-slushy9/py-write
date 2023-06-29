63aef04d7b458098d65c1d9bc7fe85f8c6638e1a
blue-slushy9 <ra.cruz971@gmail.com>
2023-06-28 17:21:24 -0700

    Cleaned up some stuff, made it more clear and legible. Out of time for now...

--- write.py
+++ write.py
@@ -1,17 +1,19 @@
 from os import path, makedirs
 
-# Create the test_files directory if it doesn't already exist;
-
+# This is the directory where we will be storing our original file and its
+# copies, we assign it to a descriptive variable for efficiency;
 directory = 'C:\\Users\\slushy\\Desktop\\Python\\write.py\\test_files\\'
 
+# Checks the filepath to see if the directory exists; if it does NOT exist...
 if not path.exists(directory):
+    # then create it;
     makedirs(directory)
 
-# Create the original text file if it doesn't already exist;
+# Create the original text file if it doesn't already exist, this is the one
+# we will copy from to create the ten duplicate (besides their names) files;
+orig_file = 'original.txt'
 
-filename = 'original.txt'
-
-filepath = 'C:\\Users\\slushy\\Desktop\\Python\\write.py\\test_files\\'
+#filepath = 'C:\\Users\\slushy\\Desktop\\Python\\write.py\\test_files\\'
 
 orig_full_path = 'C:\\Users\\slushy\\Desktop\\Python\\write.py\\test_files\\original.txt'
 
@@ -25,12 +27,12 @@ if not path.exists(orig_full_path):
     file.close()
 
     print()
-    print(f'{filename} was created and written successfully.')
+    print(f'{orig_file} was created and written successfully.')
     print()
 
 elif path.exists(orig_full_path):
     print()
-    print(f'File {filename} already exists in the directory {filepath}.')
+    print(f'File {orig_file} already exists in the directory {directory}.')
     print()
 
 else:
@@ -45,7 +47,7 @@ with open(orig_full_path, 'r') as orig_file:
     content = orig_file.read()
 
 for i in range(1, 11):
-    new_full_path = (f'{filepath}copy_file{i}.txt')
+    new_full_path = (f'{directory}copy_file{i}.txt')
     
     if not path.exists(new_full_path):
         with open(new_full_path, 'w') as new_file:
@@ -55,6 +57,6 @@ for i in range(1, 11):
             print(f'copy_file{i} was created and written successfully.')
 
     elif path.exists(new_full_path):
-        print(f'copy_file{i} already exists in {filepath}.')
+        print(f'copy_file{i} already exists in {directory}.')
 
 orig_file.close()
